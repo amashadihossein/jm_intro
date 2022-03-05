72d17c7f92b7962100c7e037e29bd175814e38d0
amashadihossein <amashadihossein@gmail.com>
2022-03-04 17:50:34 -0800

    Added pbc data

---
+++ global.R
@@ -0,0 +1,24 @@
+library(ggplot2)
+library(patchwork)
+library(dplyr)
+library(survival)
+library(survminer)
+library(JM)
+
+library(conflicted)
+conflict_prefer("select", "dplyr")
+conflict_prefer("area", "patchwork")
+
+data("pbc2")
+
+gtsummary::theme_gtsummary_journal("jama")
+theme_set(theme_bw())
+custom_theme <- function(){
+  theme_fn <- theme(legend.position='bottom',
+                    legend.direction='horizontal')
+  invisible(theme_fn)
+}
+
+
+
+
